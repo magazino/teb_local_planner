20c6e87f5abed01f9615d4f2b770a1f4a7a8e89c
Marco Bassa <bassa@magazino.eu>
2019-06-14 13:07:55 +0200

    SW-20081 Better discretization of hte costmap check

--- src/optimal_planner.cpp
+++ src/optimal_planner.cpp
@@ -1226,29 +1226,33 @@ bool TebOptimalPlanner::isTrajectoryFeasible(base_local_planner::CostmapModel* c
       }
       return false;
     }
-
-    // check if distance between two poses is higher than the robot radius and interpolate in that case
+    // checks if the distance between two poses is higher than the robot radius or the orientation diff is bigger than the specified threshold
+    // and interpolates in that case.
     // (if obstacles are pushing two consecutive poses away, the center between two consecutive poses might coincide with the obstacle ;-)!
     if (i<look_ahead_idx)
     {
-      double delta_rot = g2o::normalize_theta(teb().Pose(i+1).theta()-teb().Pose(i).theta());
-      double delta_dist = (teb().Pose(i+1).position()-teb().Pose(i).position()).norm();
-      if(fabs(delta_rot) > M_PI / 2.0 || delta_dist > 2.0 * inscribed_radius)
+      double delta_rot = g2o::normalize_theta(g2o::normalize_theta(teb().Pose(i+1).theta()) -
+                                              g2o::normalize_theta(teb().Pose(i).theta()));
+      Eigen::Vector2d delta_dist = teb().Pose(i+1).position()-teb().Pose(i).position();
+      if(fabs(delta_rot) > cfg_->trajectory.min_resolution_collision_check_angular || delta_dist.norm() > inscribed_radius)
       {
-        ROS_WARN_STREAM("Detected a discontinuity in the trajectory, delta rot: "<<delta_rot<<" delta dist: "<<delta_dist);
-        return false;
-      }
-      if (delta_dist > inscribed_radius || fabs(delta_rot) > M_PI / 4.0)
-      {
-        // check one more time
-        PoseSE2 center = PoseSE2::average(teb().Pose(i), teb().Pose(i+1));
-        if ( costmap_model->footprintCost(center.x(), center.y(), center.theta(), footprint_spec, inscribed_radius, circumscribed_radius) == -1 )
+        int n_additional_samples = std::max(std::ceil(fabs(delta_rot) / cfg_->trajectory.min_resolution_collision_check_angular), 
+                                            std::ceil(delta_dist.norm() / inscribed_radius)) - 1;
+        PoseSE2 intermediate_pose = teb().Pose(i);
+        for(int step = 0; step < n_additional_samples; ++step)
         {
-          if (visualization_) 
+          intermediate_pose.position() = intermediate_pose.position() + delta_dist / (n_additional_samples + 1.0);
+          intermediate_pose.theta() = g2o::normalize_theta(intermediate_pose.theta() + 
+                                                               delta_rot / (n_additional_samples + 1.0));
+          if ( costmap_model->footprintCost(intermediate_pose.x(), intermediate_pose.y(), intermediate_pose.theta(),
+            footprint_spec, inscribed_radius, circumscribed_radius) == -1 )
           {
-            visualization_->publishRobotFootprintModel(center, *robot_model_);
+            if (visualization_) 
+            {
+              visualization_->publishRobotFootprintModel(intermediate_pose, *robot_model_);
+            }
+            return false;
           }
-          return false;
         }
       }
     }
